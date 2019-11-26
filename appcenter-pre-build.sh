c6ae9d32eb9877a73147536787d7ceb52be9bda2
jfelix61 <rtfire456@hotmail.com>
2019-11-26 01:46:40 -0400

    Se agrego script appcenter para procesar env vars

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Creates an .env from ENV variables for use with react-native-config
+ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
+set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env

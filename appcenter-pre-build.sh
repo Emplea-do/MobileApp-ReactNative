cf044aba3ae2ca27a741e058ef3ae5130cc7babe
Jfelix61 <jfelx@outlook.com>
2019-12-08 16:09:06 -0400

    command to see working dir

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,3 +2,6 @@
 # Creates an .env from ENV variables for use with react-native-config
 ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
 set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
+
+echo $PWD
+ls
